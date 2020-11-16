4361e819743506c2310f1b72cba353f4b2a3fcd2
Ddalkigum <sol35352000@gmail.com>
2020-11-16 16:25:50 +0900

    input answer, correct number

--- baseball.js
+++ baseball.js
@@ -7,8 +7,10 @@ const answer = document.querySelector(".answer")
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 correct_number = []
 
+
 const numberPickUp = (number) => {
     numbers = numbers.filter(value => value !== number)
+    correct_number.push(number)
 
 }
 
@@ -21,39 +23,52 @@ const createAnswerNumber = () => {
     numberPickUp(third)
     const fourth = numbers[Math.floor(Math.random() * numbers.length)];
     numberPickUp(fourth)
-    console.log(first, second, third, fourth)
+    return correct_number
 }
-
-correct.innerText = createAnswerNumber()
 
 
 const createNumber = () => {
-    a = 1
+    console.log(correct_number)
+    correct_number = createAnswerNumber()
+    correct.innerText = createAnswerNumber()
 }
 
 
 const paintNumber = number => {
+    answer_number = []
     const li = document.createElement("li")
     const span = document.createElement("span")
     const numberId = numbers.length + 1
     li.appendChild(span)
-    span.innerText = number
+    first = Math.floor(number / 1000)
+    second = Math.floor(number / 100) - (first * 10)
+    third = Math.floor(number / 10) - (first * 100) - (second * 10)
+    fourth = number - (first * 1000) - (second * 100) - (third * 10)
+    answer_number.push(first, second, third, fourth)
+    span.innerText = answer_number
     numberList.appendChild(li)
     const numberObject = {
         text: number,
         id: numberId
     }
-
     numbers.push(numberObject)
+    console.log(numbers.length)
     if (numbers.length === 10) {
         input.classList.add("hiding")
         answer.classList.remove("hiding")
     }
 }
 
+correct.innerText = createAnswerNumber()
+
 numberForm.addEventListener("submit", handleSubmit = e => {
     e.preventDefault();
     const currentValue = input.value
+    if (currentValue / 1000 < 1 || currentValue / 10000 > 1) {
+        alert("4자리숫자만 입력 가능합니다.")
+        handleSubmit()
+    }
     paintNumber(currentValue);
 
 })
+
